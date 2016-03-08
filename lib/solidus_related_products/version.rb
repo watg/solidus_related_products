3af408d4d591fed7a7a9a08d5bc05751e339bf7f
John Hawthorn <john.hawthorn@gmail.com>
2016-03-08 14:09:04 -0800

    Version 1.0.0

--- lib/solidus_related_products/version.rb
+++ lib/solidus_related_products/version.rb
@@ -9,9 +9,9 @@ module SolidusRelatedProducts
 
   module VERSION
     MAJOR = 1
-    MINOR = 1
+    MINOR = 0
     TINY  = 0
-    PRE   = 'beta0'
+    PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
