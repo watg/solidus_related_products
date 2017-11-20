2dc425645da9de7eff5a89f5f51104afa3d505b3
Daniele Palombo <daniele.palombo@gmail.com>
2017-11-20 11:53:21 +0100

    Fix generate random string for relation type name

--- spec/factories/relation_type_factory.rb
+++ spec/factories/relation_type_factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :relation_type, class: Spree::RelationType do
-    name       { generate(:random_string) }
+    name       { ('A'..'Z').to_a.sample(6).join }
     applies_to 'Spree::Product'
   end
 end
