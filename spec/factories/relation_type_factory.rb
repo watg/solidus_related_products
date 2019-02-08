d9645ea9805e138142f0f2fb5c6dda88552e8cc1
Angel Perez <iAngel.p93@gmail.com>
2019-02-08 09:37:21 -0400

    Use dynamic attributes on factories

--- spec/factories/relation_type_factory.rb
+++ spec/factories/relation_type_factory.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :product_relation_type, class: Spree::RelationType do
     name       { ('A'..'Z').to_a.sample(6).join }
-    applies_to 'Spree::Product'
+    applies_to { 'Spree::Product' }
   end
 
   factory :variant_relation_type, class: Spree::RelationType do
     name       { ('A'..'Z').to_a.sample(6).join }
-    applies_to 'Spree::Variant'
+    applies_to { 'Spree::Variant' }
   end
 end
