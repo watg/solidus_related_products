11dc988c10c6571907389be102794c598aa4e5bc
Christian Rimondi <christian.rimondi@gmail.com>
2019-05-09 14:34:32 +0200

    Copy relations_controller.rb from admin and api folders into variants
    These controllers will be used as base for the new ones

---
+++ spec/controllers/spree/admin/variants/relations_controller_spec.rb
@@ -0,0 +1,104 @@
+# frozen_string_literal: true
+
+RSpec.describe Spree::Admin::RelationsController, type: :controller do
+  stub_authorization!
+
+  let(:user)     { create(:user) }
+  let!(:product) { create(:product) }
+  let!(:other1)  { create(:product) }
+
+  let!(:relation_type) { create(:product_relation_type) }
+  let!(:relation) do
+    create(
+      :product_relation,
+      relatable: product,
+      related_to: other1,
+      relation_type: relation_type,
+      position: 0
+    )
+  end
+
+  before { stub_authentication! }
+
+  context '.model_class' do
+    it 'responds to model_class as Spree::Relation' do
+      expect(controller.send(:model_class)).to eq Spree::Relation
+    end
+  end
+
+  describe 'with JS' do
+    sign_in_as_admin!
+
+    let(:valid_params) do
+      {
+        format: :js,
+        product_id: product.id,
+        relation: {
+          related_to_id: other1.id,
+          relation_type_id: relation_type.id
+        }
+      }
+    end
+
+    let(:invalid_params) { { format: :js, product_id: product.id } }
+
+    context '#create' do
+      it 'is not routable' do
+        post :create, params: valid_params
+        expect(response.status).to be(200)
+      end
+
+      it 'returns success with valid params' do
+        expect {
+          post :create, params: valid_params
+        }.to change(Spree::Relation, :count).by(1)
+      end
+
+      it 'creates a relation with the right type' do
+        post :create, params: valid_params
+        expect(Spree::Relation.last.related_to).to eq(other1)
+      end
+
+      it 'raises error with invalid params' do
+        expect {
+          post :create, params: invalid_params
+        }.to raise_error(ActionController::ParameterMissing)
+      end
+    end
+
+    context '#update' do
+      it 'redirects to product/related url' do
+        put :update, params: { product_id: product.id, id: relation.id, relation: { discount_amount: 2.0 } }
+        expect(response).to redirect_to(spree.admin_product_path(relation.relatable) + '/related')
+      end
+    end
+
+    context '#destroy' do
+      it 'records successfully' do
+        expect {
+          delete :destroy, params: { id: relation.id, product_id: product.id, format: :js }
+        }.to change(Spree::Relation, :count).by(-1)
+      end
+    end
+
+    context '#update_positions' do
+      it 'returns the correct position of the related products' do
+        other2    = create(:product)
+        relation2 = create(
+          :product_relation, relatable: product, related_to: other2, relation_type: relation_type, position: 1
+        )
+
+        expect {
+          params = {
+            product_id: product.id,
+            id: relation.id,
+            positions: { relation.id => '1', relation2.id => '0' },
+            format: :js
+          }
+          post :update_positions, params: params
+          relation.reload
+        }.to change(relation, :position).from(0).to(1)
+      end
+    end
+  end
+end
---
+++ spec/controllers/spree/api/variants/relations_controller_spec.rb
@@ -0,0 +1,99 @@
+# frozen_string_literal: true
+
+RSpec.describe Spree::Api::RelationsController, type: :controller do
+  stub_authorization!
+  render_views
+
+  let(:user)     { create(:user) }
+  let!(:product) { create(:product) }
+  let!(:other1)  { create(:product) }
+
+  let!(:relation_type) { create(:product_relation_type) }
+  let!(:relation) do
+    create(
+      :product_relation,
+      relatable: product,
+      related_to: other1,
+      relation_type: relation_type,
+      position: 0
+    )
+  end
+
+  before { stub_authentication! }
+
+  context 'model_class' do
+    it 'responds to model_class as Spree::Relation' do
+      expect(controller.send(:model_class)).to eq Spree::Relation
+    end
+  end
+
+  describe 'with JSON' do
+    sign_in_as_admin!
+
+    let(:valid_params) do
+      {
+        format: :json,
+        product_id: product.id,
+        relation: {
+          related_to_id: other1.id,
+          relation_type_id: relation_type.id
+        },
+        token: user.spree_api_key
+      }
+    end
+
+    context '#create' do
+      it 'creates the relation' do
+        post :create, params: valid_params
+        expect(response.status).to eq(201)
+      end
+
+      it 'responds 422 error with invalid params' do
+        post :create, params: { product_id: product.id, token: user.spree_api_key, format: :json }
+        expect(response.status).to eq(422)
+      end
+    end
+
+    context '#update' do
+      it 'succesfully updates the relation ' do
+        params = {
+          format: :json,
+          product_id: product.id,
+          id: relation.id,
+          relation: { discount_amount: 2.0 }
+        }
+        expect {
+          put :update, params: params
+        }.to change { relation.reload.discount_amount.to_s }.from('0.0').to('2.0')
+      end
+    end
+
+    context '#destroy with' do
+      it 'records successfully' do
+        expect {
+          delete :destroy, params: { id: relation.id, product_id: product.id, token: user.spree_api_key, format: :json }
+        }.to change(Spree::Relation, :count).by(-1)
+      end
+    end
+
+    context '#update_positions' do
+      it 'returns the correct position of the related products' do
+        other2    = create(:product)
+        relation2 = create(
+          :product_relation, relatable: product, related_to: other2, relation_type: relation_type, position: 1
+        )
+
+        expect {
+          params = {
+            product_id: product.id,
+            id: relation.id,
+            positions: { relation.id => '1', relation2.id => '0' },
+            format: :json
+          }
+          post :update_positions, params: params
+          relation.reload
+        }.to change(relation, :position).from(0).to(1)
+      end
+    end
+  end
+end
