dd9f80ebc65db7a32ceae0180a29e92f262288b2
Alessio Rocco <alessio.rocco.lt@gmail.com>
2020-04-02 15:24:05 +0200

    Fix factory relation_type associations
    The association "relation_type" of the factories "product_relation" and
    "variant_relation" was wrong.

--- lib/solidus_related_products/factories.rb
+++ lib/solidus_related_products/factories.rb
@@ -4,13 +4,13 @@ FactoryBot.define do
   factory :product_relation, class: Spree::Relation do
     association :relatable, factory: :product
     association :related_to, factory: :product
-    relation_type { 'Spree::Product' }
+    association :relation_type, factory: :product_relation_type
   end
 
   factory :variant_relation, class: Spree::Relation do
     association :relatable, factory: :product
     association :related_to, factory: :variant
-    relation_type { 'Spree::Variant' }
+    association :relation_type, factory: :variant_relation_type
   end
 
   factory :product_relation_type, class: Spree::RelationType do
