301fc8f5f713a7ac734678a9c4e9eb58e9ebe082
Alessio Rocco <alessio.rocco.lt@gmail.com>
2020-04-02 15:22:59 +0200

    Move factories under lib folder
    This way, they can also be used by the consumers of this extension.

--- lib/solidus_related_products/factories.rb
+++ lib/solidus_related_products/factories.rb
@@ -1,4 +1,27 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  factory :product_relation, class: Spree::Relation do
+    association :relatable, factory: :product
+    association :related_to, factory: :product
+    relation_type { 'Spree::Product' }
+  end
+
+  factory :variant_relation, class: Spree::Relation do
+    association :relatable, factory: :product
+    association :related_to, factory: :variant
+    relation_type { 'Spree::Variant' }
+  end
+
+  factory :product_relation_type, class: Spree::RelationType do
+    name { ('A'..'Z').to_a.sample(6).join }
+    applies_from { 'Spree::Product' }
+    applies_to { 'Spree::Product' }
+  end
+
+  factory :variant_relation_type, class: Spree::RelationType do
+    name       { ('A'..'Z').to_a.sample(6).join }
+    applies_from { 'Spree::Product' }
+    applies_to { 'Spree::Variant' }
+  end
 end
