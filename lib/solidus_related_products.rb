58ff7015cb29ffa9c8e5a143355c9668d4f9bc47
Alberto Vena <kennyadsl@gmail.com>
2021-02-05 13:18:31 +0100

    Update with solidus extension .

--- lib/solidus_related_products.rb
+++ lib/solidus_related_products.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-require 'solidus_core'
-require 'solidus_backend'
-require 'solidus_support'
+require 'solidus_related_products/configuration'
 require 'solidus_related_products/version'
 require 'solidus_related_products/engine'
 require 'deface'
